38b29a6ff8cc20ed411ebd7562734f357311303c
steeven-louk <steevenlouk.sl@gmail.com>
2023-04-07 13:19:19 +0200

    ajout du composant card

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <>
+        <div className="card"style={{ 'width':'400px' }}>
+            <div className="card-header position-relative">
+                <img src="./assets/Asset21.png" alt="" className="card-img-top img-fluid object-fit-cover" />
+                <span className="bg-success position-absolute bottom-0 start-0 fw-bold text-warning text-capitalize">category</span>
+            </div>
+            <div className="card-body bg-tertiary">
+                <h5 className="card-title">
+                Title goes here Title goes here Title goes here Title goes here                 </h5>
+
+                <p className="card-desc text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque tortor sit amet condimentum porttitor. Integer augue urna, volutpat in sapien a, convallis commodo quam.                </p>
+            </div>
+            <div className="card-footer d-flex justify-content-between align-items-center">
+                <span>24/05/2012</span>
+                <span>By Ishi</span>
+            </div>
+        </div>
+    </>
   )
 }
 
--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Card from '../../components/card/Card'
 
 const HomePage = () => {
   return (
     <div className='home'>
-        <div className="container">
+        <div className="px-5">
             <div className="h2 text-center text-capitalize my-5">latest blogs</div>
+
+            <div className="card-container d-flex wrap gap-3 w-full">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+
+            <button className="btn btn-success mt-5 text-warning align-items-center text-center mx-auto text-capitalize fw-semibold d-flex">read more</button>
         </div>
     </div>
   )
